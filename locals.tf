9bced5827f9b82056ff6c4f942416a1618d9f475
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-09-14 10:29:05 +0200

    feat: multiple subnets can now be associated with a single route table (#38)
    * feat: multiple subnets can now be associated with a single route table
    
    * feat: updated example section

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
   subnets = flatten([
     for subnet_key, subnet in try(var.vnet.subnets, {}) : {
-
       subnet_key                 = subnet_key
       virtual_network_name       = azurerm_virtual_network.vnet.name
       address_prefixes           = subnet.cidr
@@ -10,11 +9,13 @@ locals {
       enforce_priv_link_endpoint = try(subnet.enforce_priv_link_endpoint, false)
       delegations                = try(subnet.delegations, [])
       rules                      = try(subnet.rules, {})
-      subnet_name                = "${var.naming.subnet}-${subnet_key}"
-      nsg_name                   = "${var.naming.network_security_group}-${subnet_key}"
-      rt_name                    = "${var.naming.route_table}-${subnet_key}"
+      subnet_name                = try(subnet.name, "${var.naming.subnet}-${subnet_key}")
+      nsg_name                   = try(subnet.nsg.name, "${var.naming.network_security_group}-${subnet_key}")
+      rt_name                    = try(subnet.route.name, "${var.naming.route_table}-${subnet_key}")
       location                   = var.vnet.location
-      routes                     = try(subnet.routes, {})
+      routes                     = try(subnet.route.routes, {})
+      route_table                = try(subnet.route, null)
+      shd_route_table            = try(subnet.route_table, null)
     }
   ])
 }
